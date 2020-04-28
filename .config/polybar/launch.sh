e5fa07aaec82a1a38dd2b7c72734147f6a14debf
Tobias Stenberg <tobias.st.chr@gmail.com>
2020-04-28 18:26:16 +0200

    fixed polybar

---
+++ .config/polybar/launch.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# Terminate already running bar instances
+killall -q polybar
+# If all your bars have ipc enabled, you can also use 
+# polybar-msg cmd quit
+
+# Launch bar(s)
+echo "---" | tee -a /tmp/polybar_top.log
+polybar top >>/tmp/polybar_top.log 2>&1 &
+
+echo "Bars launched..."
